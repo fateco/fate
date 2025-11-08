cabd363be44aaf0aa37120bf3f2aef498bb2fe56
fateco <236334524+fateco@users.noreply.github.com>
2025-11-09 02:02:46 +0700

    change name: defualt -> skills

--- src/campaign_new/command/_3_is_defualt_skills.rs
+++ src/campaign_new/command/_3_is_defualt_skills.rs
@@ -2,13 +2,17 @@ use twilight_util::builder::command::StringBuilder;
 
 use crate::{slash::CommandLocalize, translation::all_t};
 
-pub const CAMPAIGN_NEW_SET_DEFAULT: &str = "default-skills";
+pub const CAMPAIGN_NEW_SET_SKILLS: &str = "default-skills";
 
 pub fn is_defualt_skills() -> StringBuilder {
     StringBuilder::init_localize(
-        CAMPAIGN_NEW_SET_DEFAULT,
-        all_t("campaign_new.set_default", 32),
-        all_t("campaign_new.set_default_description", 100),
+        CAMPAIGN_NEW_SET_SKILLS,
+        all_t("campaign_new.set_skills", 32),
+        all_t("campaign_new.set_skills_description", 100),
     )
     .required(true)
+    .choices([
+        (t!("campaign_new.set_skills_default"), "true"),
+        (t!("campaign_new.set_skills_custom"), "false"),
+    ])
 }
