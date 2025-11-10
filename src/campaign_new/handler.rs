75a4342511ce76dd8dc8dbbea05600d96ed87b9f
fateco <236334524+fateco@users.noreply.github.com>
2025-11-10 21:37:36 +0700

    testing - reply with hi

--- src/campaign_new/handler.rs
+++ src/campaign_new/handler.rs
@@ -2,6 +2,7 @@ use twilight_model::{
     application::interaction::{Interaction, InteractionData},
     http::interaction::{InteractionResponse, InteractionResponseData, InteractionResponseType},
 };
+use twilight_util::builder::InteractionResponseDataBuilder;
 use worker::{Env, Response, Result};
 
 use fate_internal_macro::handler;
@@ -9,13 +10,10 @@ use fate_internal_macro::handler;
 #[handler("new-campaign")]
 pub async fn campaign_new(interaction: Interaction, env: Env) -> Result<Response> {
     if let Some(InteractionData::ApplicationCommand(data)) = interaction.data {
-        println!("{:?}", data);
-        // Response::from_json(&InteractionResponse {
-        //     kind: InteractionResponseType::ChannelMessageWithSource,
-        //     data: Some(InteractionResponseData),
-        // })
-        internal_error!()
-        //data.options
+        Response::from_json(&InteractionResponse {
+            kind: InteractionResponseType::ChannelMessageWithSource,
+            data: Some(InteractionResponseDataBuilder::new().content("Hi!").build()),
+        })
     } else {
         bad_request!()
     }
